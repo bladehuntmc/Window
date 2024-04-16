cccafde346ea576d31e2b6a92cf1f91df9bb3ce9
oglass <alexbirdand@gmail.com>
2024-04-16 14:47:25 -0400

    Use compileOnly instead of API

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -22,8 +22,8 @@ publishing {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    api("net.bladehunt:ReaKt:0.1.0")
-    api("net.kyori:adventure-api:4.15.0")
+    api("net.bladehunt:reakt:0.1.1")
+    compileOnly("net.kyori:adventure-api:4.15.0")
 }
 
 tasks.test {
--- minestom/build.gradle.kts
+++ minestom/build.gradle.kts
@@ -20,9 +20,8 @@ publishing {
 }
 
 dependencies {
-    api("net.minestom:minestom-snapshots:7320437640")
-    //api("net.bladehunt:ReaKt:8b0ade6")
     api(project(":core"))
+    compileOnly("net.minestom:minestom-snapshots:7320437640")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
