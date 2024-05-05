892c44e55a757ba9e5d956157ed28415db3095b8
oglass <alexbirdand@gmail.com>
2024-05-05 11:09:46 -0400

    feat: Kotlin extensions for Size2 and Int2

---
+++ core/src/main/kotlin/net/bladehunt/window/core/ext/Size2Ext.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2024 BladehuntMC
+ * Copyright 2024 oglassdev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package net.bladehunt.window.core.ext
+
+import net.bladehunt.window.core.util.Size2
+
+operator fun Size2.component1() = this.x
+operator fun Size2.component2() = this.flexX
+operator fun Size2.component3() = this.y
+operator fun Size2.component4() = this.flexY
