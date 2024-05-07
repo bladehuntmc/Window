2618e910b131c6c1ba22187ac895e344cdcb0019
oglass <alexbirdand@gmail.com>
2024-05-07 10:42:19 -0400

    feat: Reimplement columns

--- core/src/main/kotlin/net/bladehunt/window/core/Column.kt
+++ core/src/main/kotlin/net/bladehunt/window/core/Column.kt
@@ -24,8 +24,9 @@
 package net.bladehunt.window.core
 
 import net.bladehunt.window.core.exception.WindowOverflowException
-import net.bladehunt.window.core.reservation.HookReservation
+import net.bladehunt.window.core.reservation.OffsetLimitedReservation
 import net.bladehunt.window.core.reservation.Reservation
+import net.bladehunt.window.core.util.Int2
 import net.bladehunt.window.core.widget.Widget
 import net.bladehunt.window.core.widget.WidgetParent
 
@@ -38,7 +39,7 @@ abstract class Column<T> : Widget<T>(), WidgetParent<T> {
         _children.add(widget)
     }
 
-    override fun render(reservation: Reservation<T>) {
+    override fun render(reservation: Reservation<T>): Int2 {
         val flexible = _children.filter { it.size.flexY }
         var each = 0
         var remainder = 0
@@ -52,15 +53,21 @@ abstract class Column<T> : Widget<T>(), WidgetParent<T> {
         }
 
         var previousPosY = 0
-        _children.forEach { widget ->
-            val reservation = HookReservation(
-                createReservation(),
-                { res, x, y, pixel -> },
-                { res, x, y -> }
+        var maxSizeX = 0
+        _children.forEachIndexed { index, widget ->
+            val res = OffsetLimitedReservation(
+                reservation,
+                0,
+                previousPosY,
+                Int2(
+                    reservation.size.x,
+                    if (widget.size.flexX) each + (if (index < remainder) 1 else 0) else widget.size.y
+                )
             )
-            if (flexible.contains(widget)) {
-                widget.render(reservation)
-            }
+                val final = widget.render(res)
+                if (final.x > maxSizeX) maxSizeX = final.x
+                previousPosY += final.y
         }
+        return Int2(maxSizeX, previousPosY)
     }
 }
--- minestom/src/main/kotlin/net/bladehunt/window/minestom/MinestomWindow.kt
+++ minestom/src/main/kotlin/net/bladehunt/window/minestom/MinestomWindow.kt
@@ -23,87 +23,38 @@
 
 package net.bladehunt.window.minestom
 
-import net.bladehunt.kotstom.dsl.listen
 import net.bladehunt.kotstom.extension.rowSize
-import net.bladehunt.kotstom.extension.slots
-import net.bladehunt.kotstom.util.EventNodeContainerInventory
-import net.bladehunt.window.core.reservation.ArrayReservationImpl
-import net.bladehunt.window.core.reservation.HookReservation
-import net.bladehunt.window.core.reservation.Reservation
-import net.bladehunt.window.core.reservation.Resizable
+import net.bladehunt.window.core.Column
+import net.bladehunt.window.core.util.Int2
 import net.bladehunt.window.core.util.Size2
-import net.bladehunt.window.core.widget.Widget
-import net.bladehunt.window.core.widget.WidgetParent
-import net.bladehunt.window.minestom.event.MinestomEvent
+import net.bladehunt.window.minestom.inventory.InventoryReservation
+import net.bladehunt.window.minestom.inventory.WindowInventory
 import net.kyori.adventure.text.Component
-import net.minestom.server.event.inventory.InventoryPreClickEvent
 import net.minestom.server.inventory.InventoryType
-import net.minestom.server.item.ItemStack
 
 class MinestomWindow(
     inventoryType: InventoryType,
     title: Component = Component.text("Window"),
-) : WidgetParent<WindowItem>, Widget<WindowItem> {
-    val inventory = EventNodeContainerInventory(inventoryType, title).also { inventory ->
-        val rows = inventory.inventoryType.rowSize
-        inventory.eventNode().apply {
-            listen<InventoryPreClickEvent> { event ->
-                val slots = event.clickInfo.slots
-                if (slots.size == 1) {
-                    val slot = slots.first()
-                    reservation[slot % rows, slot / rows]?.second?.interact(MinestomEvent.PreClickEvent(event))
-                }
-                event.isCancelled = true
+) : Column<WindowItem>() {
+    val inventory = WindowInventory(inventoryType, title)
+
+    fun render() {
+        inventory.updateDiff { diff ->
+            val size = Int2(
+                    inventory.inventoryType.rowSize,
+                    inventory.size / inventory.inventoryType.rowSize
+                )
+            render(
+                InventoryReservation(
+                    size,
+                    diff
+                )
+            )
+            diff.itemStacks.forEachIndexed { index, itemStack ->
+                println("$index -> $itemStack")
             }
         }
     }
 
-    private val offsets = hashMapOf<Reservation<WindowItem>, Int>()
-    private val _widgets = mutableListOf<Widget<WindowItem>>()
-
-    override val reservation: Reservation<WindowItem> = MinestomInventoryReservation(inventory)
-
-    override val widgets: Collection<Widget<WindowItem>>
-        get() = _widgets.toList()
-
-    override fun createReservation(size: Size2): Reservation<WindowItem> = HookReservation(
-        ArrayReservationImpl(size), this::onSet, this::onRemove, { _, _, _ -> render() }
-    ).also { reservation ->
-        offsets[reservation] = size.y
-    }
-
-    override fun <W : Widget<WindowItem>> addWidget(widget: W) {
-        _widgets.add(widget)
-    }
-
-    override fun calculateFlex() {
-        val flexWidgets = _widgets.filter { it.reservation.size.flexY && it.reservation is Resizable }
-        flexWidgets.forEach { widget ->
-            (widget.reservation as Resizable).resize(
-                widget.reservation.size.x,
-                flexWidgets.size / this.reservation.size.y
-            )
-        }
-    }
-
-    override fun render() {
-        reservation.clear()
-        calculateFlex()
-
-        var previousPosY = 0
-        _widgets.forEach {
-            offsets[it.reservation] = previousPosY
-            previousPosY += it.reservation.size.y
-            it.render()
-        }
-    }
-
-    private fun onSet(reservation: Reservation<WindowItem>, posX: Int, posY: Int, item: WindowItem) {
-        val offset = offsets[reservation] ?: return
-        this.reservation[posX, posY + offset] = item
-    }
-    private fun onRemove(reservation: Reservation<WindowItem>, posX: Int, posY: Int) {
-        val offset = offsets[reservation] ?: return
-        this.reservation[posX, posY + offset] = ItemStack.AIR to null
-    }
+    override val size: Size2 = Size2(inventoryType.rowSize, inventoryType.size / inventoryType.rowSize)
 }
